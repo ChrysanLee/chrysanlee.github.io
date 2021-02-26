7695b58350fa042aa537b66a473fc10d14c76930
三刀结束 <2933910660@qq.com>
2021-02-26 10:27:00 +0800

    等比例缩放css

--- js/index.js
+++ js/index.js
@@ -1,142 +1,6 @@
-
-// 找多一个魂斗罗 +  超级玛丽3
-
-var gamelist = [
-    {name:"180_梦的探险者.nes",desc:"梦之探险者",img:"default.jpg"},
-    {name:"坦克大战.nes",desc:"坦克大战",img:"default.jpg"},
-    {name:"雪人兄弟.NES",desc:"雪人兄弟",img:"default.jpg"},
-    {name:"Kero Kero Keroppi no Daibouken 2 - Donuts Ike ha Oosawagi! (Japan).nes",desc:"大眼蛙",img:"default.jpg"},
-    {name:"古巴英雄.nes",desc:"古巴英雄",img:"default.jpg"},
-
-    {name:"超级玛莉.nes",desc:"超级玛莉",img:"mario.jpg"},
-    {name:"超级马莉欧兄弟3(v20090423)[Nokoh汉化].nes",desc:"超级玛丽3",img:"default.jpg"},
-
-    {name:"松鼠1.nes",desc:"松鼠1",img:"default.jpg"},
-    {name:"松鼠2.nes",desc:"松鼠2",img:"default.jpg"},
-
-    {name:"215_热血新纪录.nes",desc:"热血新纪录",img:"default.jpg"},
-    {name:"热血物语[AIR汉化].nes",desc:"热血物语",img:"default.jpg"},
-    {name:"热血篮球(v0.9)[AXI&AHE汉化].nes",desc:"热血篮球",img:"default.jpg"},
-
-    {name:"冒险岛.nes",desc:"冒险岛",img:"default.jpg"},
-    {name:"冒险岛2 [Nokoh汉化].nes",desc:"冒险岛2",img:"default.jpg"},
-    {name:"冒险岛3(v20110101)[Nokoh汉化].nes",desc:"冒险岛3",img:"default.jpg"},
-    {name:"冒险岛4.nes",desc:"冒险岛4",img:"default.jpg"},
-
-    {name:"洛克人.nes",desc:"洛克人",img:"default.jpg"},
-    {name:"洛克人2[NeoX汉化].nes",desc:"洛克人2",img:"default.jpg"},
-    {name:"洛克人3 - Dr.Wily的末日 [MS汉化].nes",desc:"洛克人3",img:"default.jpg"},
-    {name:"洛克人4-新的野心[MS汉化].nes",desc:"洛克人4",img:"default.jpg"},
-    {name:"洛克人5-布鲁斯的圈套[MS汉化].nes",desc:"洛克人5",img:"default.jpg"},
-    {name:"洛克人6-史上最长的战斗[MS汉化].nes",desc:"洛克人6",img:"default.jpg"},
-
-    // {name:"魂斗罗1 (U)30.nes",desc:"魂斗罗1",img:"default.jpg"},
-    // {name:"魂斗罗1 (U)30F.nes",desc:"魂斗罗1-F弹药",img:"default.jpg"},
-    // {name:"魂斗罗1 (U)30L.nes",desc:"魂斗罗1-L弹药",img:"default.jpg"},
-    // {name:"魂斗罗1 (U)30M.nes",desc:"魂斗罗1-M弹药",img:"default.jpg"},
-    {name:"魂斗罗1 (U)30S.nes",desc:"魂斗罗",img:"default.jpg"},
-    {name:"魂斗罗2 (U)30S.nes",desc:"超级魂斗罗",img:"default.jpg"},
-
-
-    {name:"忍者神龟2.nes",desc:"忍者神龟2",img:"default.jpg"},
-    {name:"忍者神龟3.nes",desc:"忍者神龟3",img:"default.jpg"},
-
-    {name:"双截龙2.nes",desc:"双截龙2",img:"default.jpg"},
-    {name:"双截龙3.nes",desc:"双截龙3",img:"default.jpg"},
-    {name:"1943.nes",desc:"1943",img:"default.jpg"},
-
-    {name:"泡泡龙.nes",desc:"泡泡龙",img:"default.jpg"},
-    {name:"泡泡龙2.nes",desc:"泡泡龙2",img:"default.jpg"},
-
-    // {name:"AV麻将.nes",desc:"AV麻将",img:"default.jpg"},
-    // {name:"F1赛车.nes",desc:"F1赛车",img:"default.jpg"},
-    {name:"FC原人.nes",desc:"FC原人",img:"default.jpg"},
-
-    // {name:"七宝奇谋.nes",desc:"七宝奇谋",img:"default.jpg"},
-    {name:"三只小猪.nes",desc:"三只小猪",img:"default.jpg"},
-    {name:"三目童子.nes",desc:"三目童子",img:"default.jpg"},
-    {name:"中东战争.nes",desc:"中东战争",img:"default.jpg"},
-    {name:"中华大仙.nes",desc:"中华大仙",img:"default.jpg"},
-    {name:"中国象棋.nes",desc:"中国象棋",img:"default.jpg"},
-    {name:"人间兵器.nes",desc:"人间兵器",img:"default.jpg"},
-    {name:"企鹅梦物语.nes",desc:"企鹅梦物语",img:"default.jpg"},
-    {name:"俄罗斯方块.nes",desc:"俄罗斯方块",img:"default.jpg"},
-    {name:"充气狗.nes",desc:"充气狗",img:"default.jpg"},
-    {name:"兵蜂.nes",desc:"兵蜂",img:"default.jpg"},
-    {name:"功夫.nes",desc:"功夫",img:"default.jpg"},
-    // {name:"加纳战机(无限命+无限雷 hack by 小粽子).nes",desc:"加纳战机无限命无限雷",img:"default.jpg"},
-    // {name:"加纳战机.nes",desc:"加纳战机",img:"default.jpg"},
-    // {name:"南京企鹅.nes",desc:"南京企鹅",img:"default.jpg"},
-
-
-    {name:"吃豆.nes",desc:"吃豆",img:"default.jpg"},
-    {name:"唐老鸭梦冒险.nes",desc:"唐老鸭梦冒险",img:"default.jpg"},
-    {name:"唐老鸭梦冒险2.nes",desc:"唐老鸭梦冒险2",img:"default.jpg"},
-    {name:"嘉蒂外传.nes",desc:"嘉蒂外传",img:"default.jpg"},
-    {name:"地底探险.nes",desc:"地底探险",img:"default.jpg"},
-
-    {name:"外星战将.nes",desc:"外星战将",img:"default.jpg"},
-    // {name:"小蜜蜂.nes",desc:"小蜜蜂",img:"default.jpg"},
-    {name:"彩虹岛.nes",desc:"彩虹岛",img:"default.jpg"},
-    {name:"影子传说.nes",desc:"影子传说",img:"default.jpg"},
-    {name:"影子传说（无敌版）by nianxu.nes",desc:"影子传说无敌版",img:"default.jpg"},
-    {name:"忍者神龟2.nes",desc:"忍者神龟2",img:"default.jpg"},
-    // {name:"忍者神龟3.nes",desc:"忍者神龟3",img:"default.jpg"},
-    {name:"忍者蛙.nes",desc:"忍者蛙",img:"default.jpg"},
-    {name:"忍者龙剑传1.nes",desc:"忍者龙剑传1",img:"default.jpg"},
-    {name:"忍者龙剑传2.nes",desc:"忍者龙剑传2",img:"default.jpg"},
-    {name:"忍者龙剑传3.nes",desc:"忍者龙剑传3",img:"default.jpg"},
-    {name:"快乐猫.nes",desc:"快乐猫",img:"default.jpg"},
-    {name:"快打旋风 [老代汉化].nes",desc:"快打旋风汉化",img:"default.jpg"},
-    {name:"快打旋风.nes",desc:"快打旋风",img:"default.jpg"},
-    {name:"怪鸭历险.nes",desc:"怪鸭历险",img:"default.jpg"},
-    {name:"恶魔城.nes",desc:"恶魔城",img:"default.jpg"},
-    {name:"成龙之龙.nes",desc:"成龙之龙",img:"default.jpg"},
-    {name:"拆屋工.nes",desc:"拆屋工",img:"default.jpg"},
-    // {name:"摩托车大赛.nes",desc:"摩托车大赛",img:"default.jpg"},
-    {name:"敲冰块.nes",desc:"敲冰块",img:"default.jpg"},
-    {name:"最终任务.nes",desc:"最终任务",img:"default.jpg"},
-    {name:"未来战士.nes",desc:"未来战士",img:"default.jpg"},
-
-    // {name:"柯拉米世界2.nes",desc:"柯拉米世界2",img:"default.jpg"},
-    // {name:"桌球.nes",desc:"桌球",img:"default.jpg"},
-    // {name:"沙罗曼蛇.nes",desc:"沙罗曼蛇",img:"default.jpg"},
-
-    {name:"激龟快打.nes",desc:"激龟快打",img:"default.jpg"},
-    {name:"火之鸟.nes",desc:"火之鸟",img:"default.jpg"},
-    {name:"火箭车.nes",desc:"火箭车",img:"default.jpg"},
-    {name:"炸弹人.nes",desc:"炸弹人",img:"default.jpg"},
-    {name:"热血格斗传说.nes",desc:"热血格斗传说",img:"default.jpg"},
-    {name:"特救指令.nes",desc:"特救指令",img:"default.jpg"},
-    // {name:"王子外传.nes",desc:"王子外传",img:"default.jpg"},
-    {name:"究极虎.nes",desc:"究极虎",img:"default.jpg"},
-    {name:"绿色兵团.nes",desc:"绿色兵团",img:"default.jpg"},
-    {name:"脱狱.nes",desc:"脱狱",img:"default.jpg"},
-    {name:"脱狱2.nes",desc:"脱狱2",img:"default.jpg"},
-    {name:"荒野大镖客.nes",desc:"荒野大镖客",img:"default.jpg"},
-    {name:"菲力猫.nes",desc:"菲力猫",img:"default.jpg"},
-    // {name:"蝙蝠侠.nes",desc:"蝙蝠侠",img:"default.jpg"},
-    {name:"西游记.nes",desc:"西游记",img:"default.jpg"},
-    {name:"赤影战士.nes",desc:"赤影战士",img:"default.jpg"},
-    // {name:"赤色要塞.nes",desc:"赤色要塞",img:"default.jpg"},
-
-    {name:"越野机车.nes",desc:"越野机车",img:"default.jpg"},
-    {name:"踢王.nes",desc:"踢王",img:"default.jpg"},
-    {name:"顽皮狗.nes",desc:"顽皮狗",img:"default.jpg"},
-    {name:"马戏团.nes",desc:"马戏团",img:"default.jpg"},
-
-    {name:"魔鬼总动员.nes",desc:"魔鬼总动员",img:"default.jpg"},
-    {name:"鳄鱼先生.nes",desc:"鳄鱼先生",img:"default.jpg"},
-    {name:"鸟人战队.nes",desc:"鸟人战队",img:"default.jpg"},
-    // {name:"魂斗罗.nes",desc:"魂斗罗",img:"default.jpg"},
-    {name:"筋肉人[Madcell汉化].nes",desc:"筋肉人-摔角大赛",img:"default.jpg"},
-    {name:"救火英雄[cslrxyz汉化].nes",desc:"救火英雄",img:"default.jpg"},
-    {name:"口袋妖怪四合一[火星电子].NES",desc:"口袋妖怪",img:"default.jpg"}
-]
-
 // ---------------方向盘
 // 操作盘直径，中心方向指示盘直径，最大拖拽距离
-var joystickLen = 7, insideLen = 2, maxDragLen = 3.5;
+var joystickLen = 4.9, insideLen = 1.4, maxDragLen = 2.45;
 var htmlRem = parseInt($("html").css("font-size").replace("px",""));// html根标签的字体大小
 var dragLen = $("html").width()/htmlRem;// 拖动区域宽度
 var $joystick = $("#joystick") // 操作盘对象
@@ -144,27 +8,27 @@ var $drag = $("#drag") // 拖动有效区域对象
 var $inside = $("#inside");     // 白色内盘对象
 
 // 初始化控件大小（宽度、高度）
-$joystick.css({"height":(joystickLen-0.5*2) + 'rem',
-    "width":(joystickLen-0.5*2) + 'rem',
-    "marginLeft": '-130px',
-    "marginBottom": '110px'});    // 0.5*2是边框
+$joystick.css({"height":(joystickLen-0.35*2) + 'rem',
+    "width":(joystickLen-0.35*2) + 'rem',
+    "marginLeft": '2.3rem',
+    "marginBottom": '5.3rem'});    // 0.5*2是边框
 $drag.css({
     // "height":dragLen + 'rem',
     // "width":dragLen + 'rem'
 });
-$inside.css({"height":(insideLen - 0.3*2) + 'rem',
-    "width":(insideLen - 0.3*2) + 'rem',
-    "margin":((joystickLen-0.5*2) - insideLen)/2 + 'rem'});  // 高度减去边框
+$inside.css({"height":(insideLen - 0.21*2) + 'rem',
+    "width":(insideLen - 0.21*2) + 'rem',
+    "margin":((joystickLen-0.35*2) - insideLen)/2 + 'rem'});  // 高度减去边框
 
 // 下面一些的变量用于实现中心test方块的移动：开始
 var speed = 6;
 var htmlWidth = parseFloat($("html").css("width").replace("px",""));
-var htmlHeight = parseFloat($("html").css("height").replace("px",""));
 var transx=0, transy=0, stepx = 0, stepy = 0;
 var moving;
 
 var startX = htmlWidth/4; // 初始点击的位置
-var startY = htmlHeight-(insideLen+joystickLen/2)*htmlRem +20;     // 初始点击的位置
+var startY = $inside.offset().top-$inside.height()/2 + 20;     // 初始点击的位置
+// var startY =515;     // 初始点击的位置
 function dealTouch(e){
     e.preventDefault();
     var touch, touches = e.originalEvent.touches;  // 得到手指们
@@ -177,6 +41,7 @@ function dealTouch(e){
     }
     var x = touch.pageX - startX;
     var y = touch.pageY - startY;
+
     // 超出最大距离，使用数学中圆的方法，限定x，y在圆的范围
     if(x*x+y*y > (htmlRem*maxDragLen)**2){
         // 更好的办法，使用tan和sin和cos
@@ -187,9 +52,6 @@ function dealTouch(e){
         x = x<0?-tempX:tempX;
         y = y<0?-tempY:tempY;
     }
-
-    // console.log(x + '~~~~~~~' +y);
-    // $('#xy').text('x=' + x + ', y=' + y);
     switch (true) {
         case (x>22): // 右边
             btDown.directRight();
@@ -231,8 +93,6 @@ function dealTouch(e){
 // 当用户手指按下的时候
 $drag.bind("touchstart",function(e){
     // 初始化中间的小绿点的动画
-    // console.log(11)
-    // stepx = 0, stepy = 0, moving = setInterval(move, speed)
     dealTouch(e);
 })
 // 手指移动的时候
@@ -253,7 +113,7 @@ $drag.bind("touchend",function(e){
 
 
 
-
+// ---------------游戏装载
 var SCREEN_WIDTH = 256;
 var SCREEN_HEIGHT = 240;
 var FRAMEBUFFER_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
@@ -360,22 +220,21 @@ function nes_load_url(canvas_id, path) {
 
     req.send();
 }
+// ---------------游戏装载
+// ---------------点击事件
 var player = 1;
 // 定义点击事件
 var btDown = {
     select: function() {
         nes.buttonDown(player, jsnes.Controller.BUTTON_SELECT); // player key
-        // console.log("down")
     },
     start: function() {
         nes.buttonDown(player, jsnes.Controller.BUTTON_START); // player key
-        // console.log(1)
     },
     a: function() {
         nes.buttonDown(player, jsnes.Controller.BUTTON_A); // player key
     },
     b: function() {
-        console.log('b')
         nes.buttonDown(player, jsnes.Controller.BUTTON_B); // player key
     },
     directUp: function() {
@@ -394,11 +253,9 @@ var btDown = {
 var btUp = {
     select: function() {
         nes.buttonUp(player, jsnes.Controller.BUTTON_SELECT);
-        // console.log("up")
     },
     start: function() {
         nes.buttonUp(player, jsnes.Controller.BUTTON_START);
-        // console.log(2)
     },
     a: function() {
         nes.buttonUp(player, jsnes.Controller.BUTTON_A);
@@ -420,21 +277,9 @@ var btUp = {
         nes.buttonUp(player, jsnes.Controller.BUTTON_RIGHT);
     }
 }
-// 事件绑定
-$("#btSelect").on("touchstart", btDown.select)
-$("#btSelect").on("touchend", btUp.select)
-
-$("#btStart").on("touchstart", btDown.start)
-$("#btStart").on("touchend", btUp.start)
-
-$("#btA").on("touchstart", btDown.a)
-$("#btA").on("touchend", btUp.a)
-$("#btATemp").on("touchstart", btDown.a)
-$("#btATemp").on("touchend", btUp.a)
-
-// 连续按B
+// 连续按B（攻击）
 var alwaysClickBFlag = '';
-var alwaysClickB = function(){
+function alwaysClickB (){
     if(alwaysClickBFlag){
         clearInterval(alwaysClickBFlag);
         btUp.b();
@@ -447,20 +292,31 @@ var alwaysClickB = function(){
         setTimeout(btUp.b, 150)
     }, 300);
 }
-$("#btB").on("touchstart", alwaysClickB)
-$("#btBTemp").on("touchstart", alwaysClickB)
+// ---------------点击事件
+$("#btSelect").on("touchstart", btDown.select)
+$("#btSelect").on("touchend", btUp.select)
 
+$("#btPauseSelect").click(function() {
+    btDown.start();
+    setTimeout(btUp.start, 200);
+    $("#nes-canvas").hide();
+    $("#nes-select").show()
+});
+
+$("#btStart").on("touchstart", btDown.start)
+$("#btStart").on("touchend", btUp.start)
+
+// 跳跃
+$("#btA").on("touchstart", btDown.a)
+$("#btA").on("touchend", btUp.a)
+// 攻击
+$("#btB").on("touchstart", alwaysClickB)
 $("#btB").on("touchend", function () {
     if(alwaysClickBFlag){
         clearInterval(alwaysClickBFlag);
     }
 })
-$("#btBTemp").on("touchend", function () {
-    if(alwaysClickBFlag){
-        clearInterval(alwaysClickBFlag);
-    }
-})
-
+// 组合键
 $("#btAB").on("touchstart", function () {
     btDown.b();
     btDown.a();
@@ -469,8 +325,6 @@ $("#btAB").on("touchend", function () {
     btUp.a();
     btUp.b();
 })
-
-
 
 var secreenShow = {
     showGame: function () {
@@ -487,12 +341,6 @@ var secreenShow = {
     }
 }
 
-$("#btPauseSelect").click(function() {
-    btDown.start();
-    setTimeout(btUp.start, 200);
-    $("#nes-canvas").hide();
-    $("#nes-select").show()
-});
 //
 var selectGameId = undefined;
 var selectNesId = '';
@@ -572,5 +420,5 @@ var listOperateor = '<div>\n' +
     '            </div>';
 $('#nes-select').append(listOperateor);
 // 加载
-nes_load_url("nes-canvas", "./static/gameNes/超级玛莉.nes");
+nes_load_url("nes-canvas", "./static/gameNes/" + loadingGame);
 // nes_load_url("nes-canvas", "./static/gameNes/魂斗罗1 (U)30S.nes");
